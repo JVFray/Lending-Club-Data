34c480f930ebd369fdafdfc41dbbfca1c0131ccd
Jermaine Fray <JermaineV.Fray@Gmail.com>
2015-04-14 14:31:04 -0400

    Finished clallenge(need to clean)

---
+++ prob_lending_club.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+
+loansData.boxplot(column='Amount.Requested')
+plt.show()
+
+loansData.hist(column='Amount.Requested')
+plt.show()
+
+plt.figure()
+graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+plt.show()
